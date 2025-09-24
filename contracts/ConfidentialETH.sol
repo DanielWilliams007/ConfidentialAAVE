0bf5c0f7caf46305154fd120edef99c2b5894897
DanielWilliams007 <hifbs0ibdufzc@hotmail.com>
2025-09-24 19:06:08 +0800

    add eth faucet

--- contracts/ConfidentialETH.sol
+++ contracts/ConfidentialETH.sol
@@ -7,4 +7,13 @@ import {FHE, euint64} from "@fhevm/solidity/lib/FHE.sol";
 
 contract ConfidentialETH is ConfidentialFungibleToken, SepoliaConfig {
     constructor() ConfidentialFungibleToken("cETH", "cETH", "") {}
+
+    /// @notice Mint a fixed amount of cETH to the caller
+    /// @dev Anyone can call. Amount is encrypted on-chain using trivial encryption.
+    /// @return minted The encrypted amount minted
+    function faucet() external returns (euint64 minted) {
+        // 1 token with 6 decimals
+        euint64 amount = FHE.asEuint64(1_000_000);
+        minted = _mint(msg.sender, amount);
+    }
 }
